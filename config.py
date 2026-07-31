252bdab5b43232ad807edf8fc94c8c266d6140d2
rkoss-dev <digiman005100@gmail.com>
2026-07-30 20:02:50 -0500

    updated config file to fix deployment error

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 
-from dotenv import load_dotenv
+try:
+    from dotenv import load_dotenv
 
-load_dotenv()
+    load_dotenv()
+except ImportError:
+    pass
 
 
 class DevelopmentConfig:
